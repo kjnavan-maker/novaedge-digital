29db89f9f63d948604963c623d959f87a558b0e5
KJ Navaneethan <kjnavan@gmail.com>
2026-05-29 20:26:03 +0530

    Add email debug logs

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,29 +1,32 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-
-const sendEmail = async ({
-  to,
-  subject,
-  html,
-}) => {
+const sendEmail = async ({ to, subject, html }) => {
   try {
-    await transporter.sendMail({
+    console.log("Sending email...");
+    console.log("MAIL_USER:", process.env.MAIL_USER);
+    console.log("TO:", to);
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    const info = await transporter.sendMail({
       from: `"NovaEdge Digital" <${process.env.MAIL_USER}>`,
       to,
       subject,
       html,
     });
 
-    console.log(`Email sent to ${to}`);
+    console.log("Email sent:", info.messageId);
+    return true;
   } catch (error) {
-    console.error("Email Error:", error);
+    console.error("Email Error Message:", error.message);
+    console.error("Email Error Code:", error.code);
+    return false;
   }
 };
 
