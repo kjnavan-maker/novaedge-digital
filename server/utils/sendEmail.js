ad28fd8eea4b674dc98c4c64cce6b98ebc74a546
KJ Navaneethan <kjnavan@gmail.com>
2026-05-30 10:25:43 +0530

    Fix Brevo SDK usage

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,29 +1,23 @@
-const SibApiV3Sdk = require("@getbrevo/brevo");
+const brevo = require("@getbrevo/brevo");
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    const apiInstance = new brevo.TransactionalEmailsApi();
 
     apiInstance.setApiKey(
-      SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+      brevo.TransactionalEmailsApiApiKeys.apiKey,
       process.env.BREVO_API_KEY
     );
 
-    const sendSmtpEmail = {
+    await apiInstance.sendTransacEmail({
       sender: {
         name: "NovaEdge Digital",
         email: "hello.novaedgedigital@gmail.com",
       },
-      to: [
-        {
-          email: to,
-        },
-      ],
-      subject: subject,
+      to: [{ email: to }],
+      subject,
       htmlContent: html,
-    };
-
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    });
 
     console.log("Email sent successfully");
     return true;
